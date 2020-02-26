d1f717f2a5191426d21115d0f7132d9b9a81b95f
angelarrr <kwon.angela@gmail.com>
2020-02-25 19:56:10 -0500

    Fix test

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ class TestCase(unittest.TestCase):
 
     def test_calc_area(self):
         area = task.calc_area(2)
-        self.assertEqual(12.566370614359, area)
+        self.assertEqual(12.566370614359172, area)
 
     def test_first_last(self):
         list = [1, 3, 5, 7]
